3bebee60ae30eed71eb1bafc48dd4ee70b53e4fe
AlexAAA26A <wecannotsee@gmail.com>
2019-08-04 11:18:57 +0300

    Provider

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 import { render }from 'react-dom';
 import './index.css';
 import App from './components/App';
+import { Provider} from 'react-redux';
+import { ConnectedRouter} from 'connected-react-router';
 import * as serviceWorker from './serviceWorker';
 
 
 render(
+    <Provider>
+        <ConnectedRouter>
+    
     <div>
         <App />
 
     </div>,
+    </ConnectedRouter>
+
+    </Provider>,
     document.getElementById('root')
 
 );
