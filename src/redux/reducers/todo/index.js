237b54bf202b938fcdc63169664c8c07a8c10cff
AlexAAA26A <wecannotsee@gmail.com>
2019-08-05 16:50:09 +0300

    actions

--- src/redux/reducers/todo/index.js
+++ src/redux/reducers/todo/index.js
@@ -10,3 +10,34 @@ const todo= (state = initialState, action) => {
             list: state.list.concat(action.data),
         };
    
+        case 'UPDATE_ITEM':
+            const itemIndex = state.list.findIndex(item => item.id === action.data.id);
+    
+            return {
+                ...state,
+                list: state.list.map((item, index) => {
+                    if(index !== itemIndex) {
+                        return item;
+                    }
+                    return {
+                        ...item,
+                        ...action.data
+                    }
+                })
+            };
+        case 'REMOVE_ITEM':
+            return {
+                ...state,
+                list: state.list.filter(item => item.id !== action.data),
+            };
+        case 'SET_ITEMS':
+            return {
+                ...state,
+                list: action.data,
+            };
+        default:
+            return state;
+        }
+    };
+    
+    export default todo;
