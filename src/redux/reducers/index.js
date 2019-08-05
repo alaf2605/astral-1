6c8640c04e8022bc3483b7cbfb312f339415fa2e
AlexAAA26A <wecannotsee@gmail.com>
2019-08-05 16:51:08 +0300

    combinedReducers

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import { connectRouter} from 'connected-react-router'
+
+import todo from './todo';
+import  history from '../history'
+
+export default combineReducers({
+  router: connectRouter(history),
+  todo
+})
