879d14fcaba0a2af0c9237c90bb2f30ac892b940
AlexAAA26A <wecannotsee@gmail.com>
2019-08-05 16:55:59 +0300

    actions

---
+++ src/redux/actions/index.js
@@ -0,0 +1,64 @@
+
+
+export function setItems(data = [], type = 'SET_ITEMS') {
+    return (dispatch, getState) => {
+        if(data.length) {
+            dispatch({ type, data });
+            dispatch(saveDataInLocalStorage());
+        } else {
+            if(window.localStorage && window.localStorage.react_todo_list_items) {
+                dispatch({
+                    type,
+                    data: JSON.parse(window.localStorage.react_todo_list_items)
+                });
+            }
+        }
+    }
+}
+
+function saveDataInLocalStorage() {
+    return (dispatch, getState) => {
+        if(window.localStorage) {
+            window.localStorage.setItem('react_todo_list_items', JSON.stringify(getState().todo.list));
+        }
+        return;
+    }
+}
+
+export function addItem(data, history) {
+    return (dispatch, getState) => {
+        dispatch({
+            type: 'ADD_ITEM',
+            data: { id: generateID(getState().todo.list), message: data.message, complete: data.complete }
+        });
+        history.push('/');
+        dispatch(saveDataInLocalStorage());
+    }
+}
+
+function updateItem(data) {
+    return dispatch => {
+        dispatch({ type: 'UPDATE_ITEM', data });
+        dispatch(saveDataInLocalStorage());
+    }
+}
+
+export function updateMessage(data, history) {
+    return dispatch => {
+        dispatch(updateItem(data));
+        history.push('/');
+    }
+}
+
+export function toggleComplete(data) {
+    return dispatch => {
+        dispatch(updateItem({ id: data.id, message: data.message, complete: !data.complete }));
+    }
+}
+
+export function removeItem(id) {
+    return (dispatch, getState) => {
+        dispatch({ type: 'REMOVE_ITEM', data: id });
+        dispatch(saveDataInLocalStorage());
+    }
+}
