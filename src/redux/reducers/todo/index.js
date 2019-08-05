16963f0e849c7900c71cbc3363d33a5dbe5dbf26
AlexAAA26A <wecannotsee@gmail.com>
2019-08-05 16:50:00 +0300

    actions

---
+++ src/redux/reducers/todo/index.js
@@ -0,0 +1,12 @@
+const initialState = {
+    list: [],
+};
+
+const todo= (state = initialState, action) => {
+    switch (action.type) {
+    case 'ADD_ITEM':
+        return {
+            ...state,
+            list: state.list.concat(action.data),
+        };
+   
