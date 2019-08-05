7680630e4494ce69e8a9ac8d7edcc7c571ef4af6
AlexAAA26A <wecannotsee@gmail.com>
2019-08-05 16:41:51 +0300

    main page layout

---
+++ src/components/App/index.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import {
+    BrowserRouter as Router,
+    Route,
+    
+    
+} from 'react-router-dom';
+
+import List from '../List';
+import FormAdd from '../Form/Add';
+import FormEdit from '../Form/Edit';
+
+import { setItems } from '../../redux/actions/index'
+import './index.scss';
+
+ class App extends Component {
+    componentDidMount(){
+        this.props.setItems();
+        
+    }
+    render(){
+        return(
+            <Router>
+                <div className="app">
+                    <div className="app-box">
+                        <header className="app-header">
+                            <span className="app-title">Shopping Cart </span>
+
+                            </header>
+                                 <Route exact path="/" component={List} />
+                                 <Route exact path="/add" component={FormAdd} />
+                                 <Route exact path="/edit/:id" component={FormEdit} />
+
+
+                                 <footer className="app-footer">
+                                     <span classname="footer-title"/>
+                                     
+                                     
+                                 </footer>
+
+                           
+                        
+
+                        
+                    </div>
+                </div>
+            </Router>
+
+        );
+    }
+}
+
+
+
